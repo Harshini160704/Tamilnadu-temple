4cf30990a1d50808c5b211d746793ee4fe5532db
H A R S H I N I <snow@Hs-MacBook-Air.local>
2023-08-21 22:23:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route,Routes} from 'react-router-dom';
 
+//import ImageDisplay from './components/image';
+//import SearchBarWithAutocomplete from './components/file';
+import SearchPage from './components/search';
+//import MapLocation from './components/maptemp';
+import  {Home}  from './components/Home';
+import Sidebar from './components/Sidebar';
+import Footer from './components/footer';
+import { PopularTemples } from './components/Famous';
+import Contact from './components/ContactUs';
+import GoToTopButton from './components/gototop';
+import AboutPage from './components/AboutUs';
+import UserGuidePage from './components/Userguide';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/search' element={<SearchPage/>}></Route>
+      <Route path='/famous' element={<PopularTemples/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
+      <Route path='/about' element={<AboutPage/>}></Route>
+      <Route path='/userguide' element={<UserGuidePage/>}></Route>
+    </Routes>
+    <GoToTopButton/>
+    <Footer/>
+      
     </div>
+
   );
 }
 
